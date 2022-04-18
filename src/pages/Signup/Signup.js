a3a5085ae401672d8710149f4d07262427308db1
Your Name <golam.anik@northsouth.edu>
2022-04-18 23:20:46 +0600

    done

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -17,69 +17,85 @@ const Signup = () => {
     useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
   const [signInWithGoogle, googleuser, googleloading, googleerror] =
     useSignInWithGoogle(auth);
-  const handlerEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlerPassword = (e) => {
-    setPassword(e.target.value);
-  };
-  const handlerSubmit = (e) => {
-    createUserWithEmailAndPassword(email, password);
-    e.preventDefault();
-  };
-  return (
-    <div className='w-50 m-auto'>
-      <h1 className='text-center'>Registration</h1>
-      <Form onSubmit={handlerSubmit}>
-        <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type='email'
-            onBlur={handlerEmail}
-            placeholder='Enter email'
-            required
-          />
-          <Form.Text className='text-muted'>
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    if (loading || googleloading || sending) {
+      return (
+        <div class='spinner-border text-primary' role='status'>
+          <span class='visually-hidden'>Loading...</span>
+        </div>
+      );
+    }
+    let showerror;
+    if (error || errorr || googleerror) {
+      showerror = (
+        <div>
+          <p>Error: {error.message}</p>
+        </div>
+      );
+    }
+    const handlerEmail = (e) => {
+      setEmail(e.target.value);
+    };
+    const handlerPassword = (e) => {
+      setPassword(e.target.value);
+    };
+    const handlerSubmit = (e) => {
+      createUserWithEmailAndPassword(email, password);
 
-        <Form.Group className='mb-3' controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            onBlur={handlerPassword}
-            placeholder='Password'
-            required
-          />
-        </Form.Group>
+      e.preventDefault();
+    };
+    return (
+      <div className='w-50 m-auto'>
+        <h1 className='text-center'>Registration</h1>
+        <Form onSubmit={handlerSubmit}>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type='email'
+              onBlur={handlerEmail}
+              placeholder='Enter email'
+              required
+            />
+            <Form.Text className='text-muted'>
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Button
-          variant='dark'
-          type='submit'
-          className='d-block w-50 m-auto my-4'>
-          Submit
-        </Button>
-        <Button
-          onClick={() => {
-            signInWithGoogle();
-          }}
-          variant='dark'
-          className='d-block w-50 m-auto'>
-          <img
-            src='https://github.com/ProgrammingHero1/genius-car-service-direction-module-62/blob/main/src/images/social/google.png?raw=true'
-            width={'20px'}
-            alt=''
-          />
-          Google Signup
-        </Button>
-      </Form>
+          <Form.Group className='mb-3' controlId='formBasicPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              onBlur={handlerPassword}
+              placeholder='Password'
+              required
+            />
+          </Form.Group>
+          <p className='text-danger'>{showerror}</p>
+          <Button
+            variant='dark'
+            type='submit'
+            className='d-block w-50 m-auto my-4'>
+            Submit
+          </Button>
+          <Button
+            onClick={() => {
+              signInWithGoogle();
+            }}
+            variant='dark'
+            className='d-block w-50 m-auto'>
+            <img
+              src='https://github.com/ProgrammingHero1/genius-car-service-direction-module-62/blob/main/src/images/social/google.png?raw=true'
+              width={'20px'}
+              alt=''
+            />
+            Google Signup
+          </Button>
+        </Form>
 
-      <p>
-        Already have an Account? <Link to='/login'>Login Here</Link>
-      </p>
-    </div>
-  );
+        <p>
+          Already have an Account? <Link to='/login'>Login Here</Link>
+        </p>
+      </div>
+    );
 };
 
 export default Signup;
